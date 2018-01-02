befafde15efdf85b0c423386852f5b78735a0bb0
Paul Sutton <zleap@zleap.net>
2018-01-02 12:52:06 +0000

    fix show position and goto position

--- minecraftpi-gui.py
+++ minecraftpi-gui.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+0#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
 #  minecraft entry gui
@@ -53,19 +53,41 @@ def mcmessage():
 	mc.postToChat(mctxtVar.get()) # uncomment for program to work or comment to develop on a desktop
 
 def gotopos():
-	mcx = int(mcxtext.get())
-	mcy = int(mcytext.get())
-	mcz = int(mcztext.get())
-	print mcx,mcy,mcz
-	mc.player.setPos(mcppos.mcx, mcppos.mcy, mcppos.mcz) # uncomment for program to work or comment to develop on a desktop
-
-def play_position():
+	gotox = gotoxtextVar.get()
+	gotoy = gotoytextVar.get()
+	gotoz = gotoztextVar.get()
+	print gotox,gotoy,gotoz
+	mc.player.setPos(gotox, gotoy, gotoz) 
+	#clean up and delete old data in boxes
+	mcxtext.delete(0, END)
+	mcytext.delete(0, END)
+	mcztext.delete(0, END)
+		
+	#insert new player position
+	
 	x, y, z = mc.player.getPos()
-	print(x,y,z)
 	pos = x, y, z
+	
+	mcxtext.insert(END, x)
+	mcytext.insert(END, y)
+	mcztext.insert(END, z)
+	
+def play_position():
+	#clean up
+	mcxtext.delete(0, END)
+	mcytext.delete(0, END)
+	mcztext.delete(0, END)
+	
+	#insert new player position data
+	x, y, z = mc.player.getPos()
+	pos = x, y, z
+	
 	mcppos.delete(0, END)
-	mcppos.insert(END, pos)
-	#mcppos
+	mcxtext.insert(END, x)
+	mcytext.insert(END, y)
+	mcztext.insert(END, z)
+	
+
 	
 #def testbutton():
 #	print "test"
@@ -98,11 +120,11 @@ btn5 = Button( window, text ='Make world flat', command=flatworld )
 #Place buttons
 
 btn1.grid(row = 1, column = 4, padx = 5, pady = 5)
-btn2.grid(row = 4, column = 4, padx = 5, pady = 5)
-btn3.grid(row = 7, column = 4, padx = 5, pady = 5) #exit
-btn4.grid(row = 5, column = 4, padx = 5, pady = 5) # player position
-btn5.grid(row = 6, column = 4, padx = 5, pady = 5)
+btn2.grid(row = 6, column = 4, padx = 5, pady = 5) #goto
 
+btn4.grid(row = 4, column = 4, padx = 5, pady = 5) # player position
+btn5.grid(row = 8, column = 4, padx = 5, pady = 5) #flat world
+btn3.grid(row = 9, column = 4, padx = 5, pady = 5) #exit
 #define and place labels
 
 #define labels - cannot share same name as function
@@ -111,15 +133,20 @@ mcxpos = Label(window, text="x_pos ")
 mcypos = Label(window, text="y_pos: ")
 mczpos = Label(window, text="z_pos: ")
 plypos = Label(window, text="player_position ")
+gotox = Label(window, text="goto x_pos ")
+gotoy = Label(window, text="goto y_pos: ")
+gotoz = Label(window, text="goto z_pos: ")
 
 #place labels
 mcmesg.grid(row = 1, column = 1, padx = 5, pady = 5)
-
+#place lables for player pos
 mcxpos.grid(row = 3, column = 1, padx = 5, pady = 5)
 mcypos.grid(row = 3, column = 2, padx = 5, pady = 5)
 mczpos.grid(row = 3, column = 3, padx = 5, pady = 5)
-plypos.grid(row = 5, column = 1, padx = 5, pady = 5)
-
+#place labels for goto pos
+gotox.grid(row = 5, column = 1, padx = 5, pady = 5)
+gotoy.grid(row = 5, column = 2, padx = 5, pady = 5)
+gotoz.grid(row = 5, column = 3, padx = 5, pady = 5)
 
 #create text entry file name boxes
 
@@ -140,6 +167,16 @@ mcztext = Entry(window, textvariable=mcztextVar)
 mcpposVar = StringVar()
 mcppos = Entry(window, textvariable=mcpposVar)
 
+#define entry boxes for goto position
+
+gotoxtextVar = StringVar()
+gotoxtext = Entry(window, textvariable=gotoxtextVar)
+
+gotoytextVar = StringVar()
+gotoytext = Entry(window, textvariable=gotoytextVar)
+
+gotoztextVar = StringVar()
+gotoztext = Entry(window, textvariable=gotoztextVar)
 
 
 #place entry boxes
@@ -149,7 +186,14 @@ mcmesgtext.grid(row = 1, column = 2,) # text for post to chat
 mcxtext.grid(row = 4, column = 1, padx = 2, pady = 2) #xpos
 mcytext.grid(row = 4, column = 2, padx = 2, pady = 2) #ypos
 mcztext.grid(row = 4, column = 3, padx = 2, pady = 2) #zpos
-mcppos.grid(row = 5, column = 2, padx = 2, pady = 2) #zpos
+#mcppos.grid(row = 5, column = 2, padx = 2, pady = 2) #zpos
+
+#place entry boxes for goto position
+
+gotoxtext.grid(row = 6, column = 1, padx = 2, pady = 2) #xpos
+gotoytext.grid(row = 6, column = 2, padx = 2, pady = 2) #ypos
+gotoztext.grid(row = 6, column = 3, padx = 2, pady = 2) #zpos
+
 
 window.mainloop()
 	
